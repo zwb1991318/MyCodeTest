c43c0a8f155f6102f3564f3e26945fe64db352be
zwb <540305341@qq.com>
2020-03-03 23:22:46 +0800

    fix same code

--- Self/crawler/taobao_test.py
+++ Self/crawler/taobao_test.py
@@ -3,13 +3,14 @@ import re
 import requests
 
 URL_PRE = "https://s.taobao.com/search?q="
-URL_SUF = "&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20200302&ie=utf8"
+URL_SUF = "&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20200303&ie=utf8"
 URL_DELT = "&bcoffset=3&ntoffset=3&p4ppushleft=1%2C48&s="
 DELT_NUM = 44
 
 def get_html(url_tb):
     """get html of mackbook pro 16 from taobao.com"""
     print(url_tb)
+    mac_list = []
     try:
         response = requests.get(url_tb, timeout=30)
         response.raise_for_status()
@@ -18,36 +19,40 @@ def get_html(url_tb):
     except ValueError:
         return ""
 
-def parse_html(mac_list,html_tb):
+def parse_html(html_tb):
     """put html_tb to macbook pro 16 lsit"""
     print(html)
+    mac_list = []
     try:
         plt = re.findall(r'\"view_price\"\:\"[\d\.]*\"',html_tb)
         tlt = re.findall(r'\"raw_title\"\:\".*?\"',html_tb)
         for i in range(len(plt)):
             price = eval(plt[i].split(':')[1])
-            title = eval(tlt[i].split(':')[1])         
+            title = eval(tlt[i].split(':')[1])
+            mac_list.append([price,title])
     except ValueError:
         print("valuie error")
-def print_macbook(mac_list):
+
+    return mac_list
+
+def print_macbook(maclist):
     """print macbook pro 16 list from taobao"""
-    print(mac_list)
+    print(maclist)
 
 if __name__ == "__main__":
     MACBOOK = "macbookpro16"
     SEARCH_URL = URL_PRE + MACBOOK + URL_SUF
-    mac_list = []
     for i in range(3):
         try:
             if i == 0:
                 url = SEARCH_URL
             else:
-                url = SEARCH_URL + URL_DELT + DELT_NUM * i
+                url = SEARCH_URL + URL_DELT + str(DELT_NUM * i)
 
             html = get_html(url)
-            parse_html(mac_list,html)
+            info_list = parse_html(html)
         except ValueError:
             print("url failed")
             continue
     
-    print_macbook(MACLIST)
+    print_macbook(info_list)
